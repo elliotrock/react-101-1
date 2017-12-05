6b21ca1c20ddd87e8b2000e32b3f10c17df0f31a
elliotrock <elliotrockcom@gmail.com>
2017-12-05 18:27:27 +1100

    Updated: readme.md

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul className="vertical-list" >
+          <li>text 1</li>
+          <li>text 2</li>
+          <li>text 3</li>
+          <li>text 4</li>
+          <li>text 5</li>
+          <li>text 1</li>
+          <li>text 2</li>
+          <li>text 3</li>
+          <li>text 4</li>
+          <li>text 5</li>
+        </ul> 
+      </div>
+    );
+  }
+}
+
+export default App;
